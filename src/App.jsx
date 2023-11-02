d44d1bfa94d6c949c5ebe3a867c8109c41c7f74e
NatasjaK <natasja.kauppi@gmail.com>
2023-11-02 15:02:48 +0100

    Setting up settings

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import './App.css'
+
+function App() {
+  return (
+   
+      <h1>Vite + React</h1>
+  )
+}
+
+export default App
